d43d4bd0872178bcbbf9f564a54cfabf14567d4f
Luc Awater <lucawater@gmail.com>
2017-01-06 11:17:21 +0100

    Add ppp project for testing

---
+++ ppp/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Politieke Partijen</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Quicksand|Source+Sans+Pro|Work+Sans:400,600" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6711094/7499372/css/fonts.css" />
+    <link rel="stylesheet" href="/ppp/app.css">
+  </head>
+  <body>
+    <section class="page-top">
+      <img src="images/cda-logo.png" alt="CDA logo" />
+
+      <nav>
+        <ul>
+          <li><a href="#">De Partijen</a></li>
+          <li><a href="#">Stemwijzers</a></li>
+        </ul>
+      </nav>
+    </section>
+    <main>
+      <section>
+        <div class="section__body">
+          <p><small>Volledige naam</small>Christen-Democratisch Appel</p>
+          <p><small>Opgericht op</small>19 december 1950</p>
+          <p><small>Website</small>www.cda.nl</p>
+
+          <p>Eerlijk delen, vast werk, goed wonen, een gezond leven, toegang tot scholing, emancipatie, recht op zeggenschap - het is de rode draad van de sociaaldemocratie in de strijd voor een rechtvaardige wereld.</p>
+
+          <a href="#">Terug naar overzicht</a>
+        </div>
+      </section>
+    </main>
+  </body>
+</html>
