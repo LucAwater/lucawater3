d4b7a9905d8f9a57599c93da4947e80bc7f8240d
Luc Awater <lucawater@gmail.com>
2017-01-08 21:05:09 +0100

    ppp: add panels

--- ppp/views/party/main.php
+++ ppp/views/party/main.php
@@ -3,29 +3,20 @@
     <div class="panel">
       <div class="media media-small">
         <div class="media__media">
-          <img src="images/pdf.svg" alt="pdf icon">
+          <figure>
+
+          </figure>
         </div>
         <div class="media__body">
-          <p>Volledig verkiezingsprogramma</p>
-          <small>Download het programma als pdf</small>
-        </div>
-      </div>
-    </div>
-    <div class="panel">
-      <div class="media media-small">
-        <div class="media__media">
-          <img src="images/pdf.svg" alt="pdf icon">
-        </div>
-        <div class="media__body">
-          <p>Beknopte versie</p>
-          <small>Download het programma als pdf</small>
+          <p>Gert-Jan Segers</p>
+          <small>Partijleider</small>
         </div>
       </div>
     </div>
 
-    <p><small>Volledige naam</small>Christen-Democratisch Appel</p>
+    <p><small>Volledige naam</small>Christen Unie</p>
     <p><small>Opgericht op</small>19 december 1950</p>
-    <p><small>Website</small>www.cda.nl</p>
+    <p><small>Website</small>www.christenunie.nl</p>
 
     <p>Eerlijk delen, vast werk, goed wonen, een gezond leven, toegang tot scholing, emancipatie, recht op zeggenschap - het is de rode draad van de sociaaldemocratie in de strijd voor een rechtvaardige wereld.</p>
 
