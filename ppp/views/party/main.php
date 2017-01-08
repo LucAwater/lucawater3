3c2b6ff3cb599e57f8914868797fcd5465727297
Luc Awater <lucawater@gmail.com>
2017-01-08 19:00:54 +0100

    ppp: add panels

--- ppp/views/party/main.php
+++ ppp/views/party/main.php
@@ -1,5 +1,28 @@
 <section class="section-text">
   <div class="section-body">
+    <div class="panel">
+      <div class="media media-small">
+        <div class="media__media">
+          <img src="images/pdf.svg" alt="pdf icon">
+        </div>
+        <div class="media__body">
+          <p>Volledig verkiezingsprogramma</p>
+          <small>Download het programma als pdf</small>
+        </div>
+      </div>
+    </div>
+    <div class="panel">
+      <div class="media media-small">
+        <div class="media__media">
+          <img src="images/pdf.svg" alt="pdf icon">
+        </div>
+        <div class="media__body">
+          <p>Beknopte versie</p>
+          <small>Download het programma als pdf</small>
+        </div>
+      </div>
+    </div>
+
     <p><small>Volledige naam</small>Christen-Democratisch Appel</p>
     <p><small>Opgericht op</small>19 december 1950</p>
     <p><small>Website</small>www.cda.nl</p>
